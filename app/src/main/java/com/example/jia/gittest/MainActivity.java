2e3b26af69cd45b1a565dea620cac67641fe6b37
jia <2572745567@qq.com>
2017-04-18 19:47:45 +0800

    jia Commit initial

---
+++ app/src/main/java/com/example/jia/gittest/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.jia.gittest;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.i("fsdfsd", "fdsfs");
+    }
+}
